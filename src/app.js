aca20a0a53c0c09f02176062678f5371b36e115b
Supportdnxai <support@dnxaisolutions.com>
2025-11-30 14:12:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import dealerRoutes from "./routes/dealerRoutes.js";
 import farmerRoutes from "./routes/farmerRoutes.js";
+import maoRoutes from "./routes/maoRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/dealers", dealerRoutes);
 app.use("/api/farmers", farmerRoutes);
+app.use("/api/mao", maoRoutes);
 
 app.get("/", (req, res) => {
   res.send("Fertilizer Distribution API is running...");
